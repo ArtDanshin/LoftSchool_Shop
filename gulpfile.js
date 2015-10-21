911d4f01f1d724ca017049f1d05c8423aad6cb3b
SilverDragoon <silvir007@gmail.com>
2015-10-21 20:03:58 +0300

    SCSS, Compass, Jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,79 @@
-var gulp = require('gulp'),
-	browserSync = require('browser-sync');
+/* ----- plugins ----- */
 
-gulp.task('server', function() {
-	browserSync({
+var gulp 		= require('gulp'),
+	browserSync = require('browser-sync'),
+	jade        = require('jade'),
+	compass 	= require('compass');
+
+/* -----  paths ----- */
+
+var paths = {
+		jade : {
+			location	: './app/markups/**/*.jade',
+			compiled	: './app/markups/_pages/*.jade',
+			destination	: '.'
+		},
+
+		scss : {
+			location	: './app/scss/**/*scss',
+			entryPoint	: './app/css/main.css',
+		},
+
+		compass : {
+			configFile	: './config.rb',
+			cssFolder	: './app/css',
+			scssFolder	: './app/scss',
+			imgFolder	: './img'
+		},
+
+		browserSync : {
+			baseDir		: './app/',
+			watchPaths 	: ['app/*.html', 'app/css/*.css', 'app/js/*.js']
+		}
+}
+
+/* ----- Jade ----- */
+
+gulp.task('jade', function() {
+	gulp.src(paths.jade.compiled)
+		.pipe(jade({
+			pretty: '\t',
+		}))
+		.pipe(gulp.dest(paths.jade.destination));
+});
+
+/* ----- SCSS ----- */
+
+gulp.task('compass', function() {
+	gulp.src(paths.scss.location)
+		.pipe(plumber())
+		.pipe(compass({
+			config_file: paths.compass.configFile,
+			css: paths.compass.cssFolder,
+			sass: paths.compass.scssFolder,
+			image: paths.compass.imgFolder
+		}));
+});
+
+/* ----- Browser sync ----- */
+
+gulp.task('sync', function() {
+	browserSync.init({
 		port: 9000,
 		server: {
-			baseDir: "app"
+			baseDir: paths.browserSync.baseDir
 		}
 	});
 });
 
-gulp.task('watch' , function () {
-	gulp.watch([
-		'app/*.html',
-		'app/js/**/*.js',
-		'app/css/**/*.css'
-		]).on('change', browserSync.reload);
+/* ----- Watch ----- */
+
+gulp.task('watch', function(){
+	gulp.watch(paths.jade.location, ['jade']);
+	gulp.watch(paths.scss.location, ['compass']);
+	gulp.watch(paths.browserSync.watchPaths).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['server', 'watch']);
+/* ----- Default ----- */
+
+gulp.task('default', ['jade', 'compass', 'sync', 'watch']);
