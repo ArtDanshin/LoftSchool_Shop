7ba10aa5bb4226a1c89ae93b22811159b604b605
SilverDragoon <silvir007@gmail.com>
2015-11-01 16:57:12 +0300

    Добавлен слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,32 @@ var loftShop = (function () {
 
 	// Инициализация
 	var init = function () {
-		$(".sort-block__select").select2({
-			minimumResultsForSearch: Infinity
-		});
+		_selectSort();
+		_sliderPrice();
 	};
 
 	// Прослушивание событий
 	var _setUpListners = function () {
 		
 	};
+
+	// Select2 - Селектор сориторовки
+	var _selectSort = function () {
+		$(".sort-block__select").select2({
+			minimumResultsForSearch: Infinity
+		});
+	}
+
+	// jQuery UI Slider - Слайдер фильтра цены
+	var _sliderPrice = function () {
+		console.log('I am hero')
+		$( ".filter-price" ).slider({
+			range: true,
+	 		min: 0,
+			max: 500,
+			values: [ 75, 300 ]
+		});
+	}
 
 	return {
 		init: init
