2c1decd096ea26dfb234afa68e9a8e887415130f
SilverDragoon <silvir007@gmail.com>
2015-11-01 18:21:51 +0300

    Написан аккордеон для фильтров

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,7 @@ var loftShop = (function () {
 	var init = function () {
 		_selectSort();
 		_sliderPrice();
+		_accordeon();
 	};
 
 	// Прослушивание событий
@@ -20,13 +21,32 @@ var loftShop = (function () {
 
 	// jQuery UI Slider - Слайдер фильтра цены
 	var _sliderPrice = function () {
-		console.log('I am hero')
 		$( ".filter-price" ).slider({
 			range: true,
 	 		min: 0,
 			max: 500,
 			values: [ 75, 300 ]
 		});
+	}
+
+	// Аккордеон
+	var _accordeon = function () {
+		$('.filter-item-head__title').on('click', function(event) {
+			event.preventDefault();
+			var $this = $(this),
+				item = $this.closest('.filters__item'),
+				list = $this.closest('.filters'),
+				content = item.find('.filters-item__body'),
+				duration = 300;
+
+			if (!item.hasClass('filters__item_active')) {
+				item.addClass('filters__item_active');
+				content.stop(true, true).slideDown(duration)
+			} else {
+				item.removeClass('filters__item_active');
+				content.stop(true, true).slideUp(duration)
+			}
+		})
 	}
 
 	return {
